68a6e2b748e7f841ead534c2fd279d5bc6b29b5b
robMolloy <robmolloy@hotmail.co.uk>
2021-11-30 00:45:11 +0000

    remove logs

--- components/listener.js
+++ components/listener.js
@@ -1,15 +1,6 @@
-// const q = query(collection(db, "cities"), where("state", "==", "CA"));
-// const unsubscribe = onSnapshot(q, (querySnapshot) => {
-//   const cities = [];
-//   querySnapshot.forEach((doc) => {
-//       cities.push(doc.data().name);
-//   });
-//   console.log("Current cities in CA: ", cities.join(", "));
-// });
-
 export const listener = {
-  listenToCollection({ collectionName }) {
-    const unsubscribe = this.listen({ collectionName });
+  listenToCollection({ collectionName, onChange }) {
+    const unsubscribe = this.listen({ collectionName, onChange });
     return unsubscribe;
   },
 
