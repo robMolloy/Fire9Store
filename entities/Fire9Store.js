26c6acd64235a2f692c0832e3012a817e8df066f
robMolloy <robmolloy@hotmail.co.uk>
2021-12-04 08:35:05 +0000

    fixed

--- entities/Fire9Store.js
+++ entities/Fire9Store.js
@@ -46,27 +46,4 @@ export class Fire9Store {
     return this.writeBatch(this.db);
   }
 
-  async delete(...props) {
-    if(this.isManyDocs(props[0].payload)) return await this.deleteMany(...props) 
-    return await this.deleteOne(...props);
-  }
-
-  async deleteOne({ collectionName, payload }) {
-    const id = payload;
-    const ref = this.getRef({ collectionName, id });
-    return await this.deleteDoc(ref);
-  }
-
-  async deleteMany({ collectionName, payload }) {
-    const ids = payload;
-    console.log(/*LL*/ 11, 'this', this);
-    const batch = this.getBatch();
-
-    ids.forEach((id) => {
-      const ref = this.getRef({ collectionName, id });
-      batch.delete(ref);
-    });
-
-    return await batch.commit();
-  }
 }
